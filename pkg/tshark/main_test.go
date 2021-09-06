4f7ba86a15b66ad4c8689b90629ac80eb6809e5c
monandkey <satoru070505@gmail.com>
2021-09-06 09:58:58 +0000

    [update] main_test.go
    Change the test configuration.

--- pkg/tshark/main_test.go
+++ pkg/tshark/main_test.go
@@ -5,6 +5,7 @@ import (
 	"local.packages/tshark"
 )
 
+
 func TestRunTshark(t *testing.T) {
 	type Tests struct {
 		name string
@@ -12,36 +13,33 @@ func TestRunTshark(t *testing.T) {
 		want int
 	}
 
-	tests := []Tests {
-		{
-			name: "Normal Case",
-			args: "/home/makeplantuml/container/sample/testattach.pcapng",
-			want: 17,
-		},
-		{
+	tests := Tests {
+		name: "Normal Case",
+		args: "/home/makeplantuml/container/sample/testattach.pcapng",
+		want: 17,
+	}
+
+	t.Run(tests.name, func(t *testing.T) {
+		got := tshark.RunTshark(tests.args)
+		if len(got) == 0 {
+			t.Errorf("The return value is zero.\nReturn Value: %d", len(got))
+
+		} else if (len(got) % tests.want) == 0 {
+			t.Errorf("The return value is not the expected value.\nValue: %d", len(got))
+		}
+	})
+
+	tests = Tests {
 			name: "Error Case",
 			args: "sample/testattach.pcapng",
 			want: 0,
-		},
 	}
 
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got := tshark.RunTshark(tt.args)
-			switch(tt.name) {
-				case "Normal Case":
-					if len(got) == 0 {
-						t.Errorf("The return value is zero.\nReturn Value: %d", len(got))
-
-					} else if (len(got) % tt.want) == 0 {
-						t.Errorf("The return value is not the expected value.\nValue: %d", len(got))
-					}
-
-				case "Error Case":
-					if len(got) != tt.want {
-						t.Errorf("The expected value is zero.")
-					}
-				}
-		})
-	}
+	t.Run(tests.name, func(t *testing.T) {
+		got := tshark.RunTshark(tests.args)
+		if len(got) != tests.want {
+			t.Errorf("The expected value is zero.")
+		}
+	})
 }
+
